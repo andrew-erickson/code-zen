c750c7d37bd0d9eb840f8363c9aa424cd86a5c24
Andrew Erickson <andrew.erickson321@gmail.com>
2020-05-01 04:45:11 +0000

    updated launch to add ssh keys

--- launch/code-zen.sh
+++ launch/code-zen.sh
@@ -10,7 +10,7 @@ curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh >
 
 #add .bashrc
 printf "\n%s\n\n" "$(curl https://raw.githubusercontent.com/andrew-erickson/dotfiles/14223787766c22cfd40b70ca212c8e460662f6c0/.bashrc)" >> ~/.bashrc
-echo '\n[ -r ~/.bashrc ] && . ~/.bashrc' >> ~/.bash_profile
+echo '[ -r ~/.bashrc ] && . ~/.bashrc' >> ~/.bash_profile
 
 #add vim configs
 mkdir -p ~/.vim/colors
@@ -44,12 +44,17 @@ read -p "Copy key below and paste -> https://console.aws.amazon.com/iam/home#/us
 #  User <SSH key id>
 #  IdentityFile ~/.ssh/codecommit_rsa
 
+# add the keys
+eval "$(ssh-agent -s)"
+ssh-add .ssh/github_rsa
+ssh-add .ssh/codecommit_rsa
+
 #clone code zen
-git clone --recurse-submodules git@github.com:andrew-erickson/code-zen.git
-[[ -e ~/code-zen ]] && cp -rT ~/code-zen ~/workspace && rm -rf ~/code-zen
+git clone --recurse-submodules git@github.com:andrew-erickson/code-zen.git tmp
+[[ -e ~/tmp ]] && cp -rT ~/tmp ~/code-zen && rm -rf ~/tmp
 
 #git config
-cd ~/workspace
+cd ~/code-zen
 git config --global user.name "Andrew Erickson"
 git config --global user.email andrew.erickson321@gmail.com
 git config push.recurseSubmodules on-demand
